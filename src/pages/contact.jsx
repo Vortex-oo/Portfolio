13199040be383e715570146bf31ca7ab889244a2
Vortex-oo <debjit.github@gmail.com>
2025-02-28 01:10:01 +0530

    refactor Contact component for improved layout and social media link accessibility

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -162,10 +162,10 @@ const Contact = () => {
         <div id="contacts" className="relative h-screen w-full bg-[#1e2125] text-[#d5ccbb] font-[Yesteryear] 
             flex justify-center items-center flex-col leading-none 
             px-4 sm:px-6 md:px-8 ">
-    
+
             {/* Let's Create Section */}
             <div className="w-full flex flex-col items-center lg:items-center lg:justify-center 
-                lg:text-[184px] italic font-bold mb-0 leading-[0.8] cursor-none lg:flex lg:flex-row lg:gap-14" 
+                lg:text-[184px] italic font-bold mb-0 leading-[0.8] cursor-none lg:flex lg:flex-row lg:gap-14"
                 ref={creatingRef}>
                 <div className="text-[#B5514D] text-4xl sm:text-5xl md:text-6xl lg:text-6xl 
                     font-bold italic flex flex-row sm:flex-col items-center justify-center 
@@ -177,51 +177,55 @@ const Contact = () => {
                     SOMETHING
                 </h1>
             </div>
-    
+
             {/* Meaningful Text */}
             <div className="w-full text-center lg:text-center
                 text-5xl sm:text-7xl md:text-[130px] lg:text-[175px] 
-                italic font-extrabold leading-[0.8] cursor-none mb-1 lg:mb-0" 
+                italic font-extrabold leading-[0.8] cursor-none mb-1 lg:mb-0"
                 ref={delightfulRef}>
                 <h1>MEANINGFUL</h1>
             </div>
-    
+
             {/* Together Text */}
             <div className="w-full text-center lg:text-center
                 text-4xl sm:text-4xl md:text-[40px] lg:text-[127px] 
-                italic lg:leading-[0.7] mb-0 lg:mb-3  cursor-none" 
+                italic lg:leading-[0.7] mb-0 lg:mb-3  cursor-none"
                 ref={momentsRef}>
                 together
             </div>
-    
+
             {/* Social Icons */}
-            <div className="flex flex-wrap justify-center lg:justify-centre gap-4 sm:gap-6 lg:gap-10 mt-5 w-full" 
+            <div className="flex flex-wrap justify-center lg:justify-centre gap-4 sm:gap-6 lg:gap-10 mt-5 w-full"
                 ref={locationRef}>
                 {/* Instagram */}
                 <div className="bg-white rounded-full
-                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16" 
+                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16"
                     ref={instagramRef}>
-                    <img
-                        src="https://cdn.prod.website-files.com/61b74db330d7740923e4176b/61e71dd63660128126610916_social-instagram.svg"
-                        alt="instagram"
-                        className="w-full h-full transition-transform duration-300 hover:scale-110"
-                    />
+                    <a href="https://www.instagram.com/dust_bi9?igsh=NThudGZiZW5zdWVx" target="_blank">
+                        <img
+                            src="https://cdn.prod.website-files.com/61b74db330d7740923e4176b/61e71dd63660128126610916_social-instagram.svg"
+                            alt="instagram"
+                            className="w-full h-full transition-transform duration-300 hover:scale-110"
+                        /></a>
+
                 </div>
-    
+
                 {/* LinkedIn */}
                 <div className="bg-white rounded-full
-                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16" 
+                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16"
                     ref={linkedinRef}>
-                    <img
-                        src="https://cdn.prod.website-files.com/61b74db330d7740923e4176b/61e9527d263afad4313cc5c2_Linekdin.svg"
-                        alt="linkedin"
-                        className="w-full h-full transition-transform duration-300 hover:scale-110"
-                    />
+                    <a href="https://www.linkedin.com/in/debjit-sen-a37980282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
+                        <img
+                            src="https://cdn.prod.website-files.com/61b74db330d7740923e4176b/61e9527d263afad4313cc5c2_Linekdin.svg"
+                            alt="linkedin"
+                            className="w-full h-full transition-transform duration-300 hover:scale-110"
+                        /></a>
+
                 </div>
-    
+
                 {/* Dribbble */}
                 <div className="bg-white rounded-full
-                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16" 
+                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16"
                     ref={dribbleRef}>
                     <img
                         src="https://cdn.prod.website-files.com/61b74db330d7740923e4176b/61e9527d81a5bc73ee81ff5e_Dribbble.svg"
@@ -229,12 +233,12 @@ const Contact = () => {
                         className="w-full h-full transition-transform duration-300 hover:scale-110"
                     />
                 </div>
-    
+
                 {/* GitHub */}
                 <div className="bg-white rounded-full flex justify-center items-center
-                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16" 
+                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16"
                     ref={githubRef}>
-                    <a href="https://github.com/Vortex-oo">
+                    <a href="https://github.com/Vortex-oo" target="_blank">
                         <img
                             src="https://utfs.io/f/a05b28c5-e2ef-4d71-beb2-fbd6ee89cdab-61mg60.1023x1024.png"
                             alt="github"
@@ -243,20 +247,22 @@ const Contact = () => {
                         />
                     </a>
                 </div>
-    
+
                 {/* Telegram */}
                 <div className="bg-white rounded-full flex justify-center items-center
-                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16" 
+                    w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16"
                     ref={telegramRef}>
-                    <img
-                        src="https://utfs.io/f/a8f37fe8-adf0-4d32-8299-dadc90e43c67-mhzjlr.1024x919.png"
-                        alt="telegram"
-                        className="w-7 h-7 sm:w-8 sm:h-8 md:w-9 md:h-9 lg:w-9 lg:h-9 
+                    <a href="https://t.me/b_aundule" target="_blank"  >
+                        <img
+                            src="https://utfs.io/f/a8f37fe8-adf0-4d32-8299-dadc90e43c67-mhzjlr.1024x919.png"
+                            alt="telegram"
+                            className="w-7 h-7 sm:w-8 sm:h-8 md:w-9 md:h-9 lg:w-9 lg:h-9 
                             transition-transform duration-300 hover:scale-110"
-                    />
+                        /></a>
+
                 </div>
             </div>
-    
+
             {/* Footer */}
             <footer className="absolute bottom-5 w-full text-center text-gray-400 
                 text-xs sm:text-sm lg:text-sm flex flex-col items-center">

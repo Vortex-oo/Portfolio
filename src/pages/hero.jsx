f9d962b628131c1bdd1c5e4ef261e0743d2e6610
Vortex-oo <debjit.github@gmail.com>
2025-03-10 21:35:07 +0530

    chng the space

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -205,7 +205,7 @@ const Hero = () => {
         </div>
 
         <div
-          className=' text-5xl lg:text-[184px] italic font-extrabold leading-[0.8] cursor-none'
+          className=' text-5xl lg:text-[184px] italic font-extrabold  leading-[1.6]  md:leading-[0.8] cursor-none'
           ref={delightfulRef}
           onMouseEnter={textEnter}
           onMouseLeave={textLeave}

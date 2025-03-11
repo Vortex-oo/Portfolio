e8c68dfa9b579f2497d2699f2079c6bb7c75b33b
Vortex-oo <debjit.github@gmail.com>
2025-03-11 09:41:43 +0530

    chng the padding

--- src/component/navbar2.jsx
+++ src/component/navbar2.jsx
@@ -7,7 +7,7 @@ const Navbar2 = () => {
             <h1 className='text-base font-bold'  >
                 Menu
             </h1>
-            <div className='bg-white text-black font-black  rounded-full w-3 h-3  p-3 flex justify-center items-center '  >
+            <div className='bg-white text-black font-black  rounded-full w-3 h-3  p-2 flex justify-center items-center '  >
                 <h1 className='text-base font-extrabold'  ><FaPlus /></h1>
             </div>
         </div>

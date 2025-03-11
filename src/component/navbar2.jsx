00f9c24e2b223c2b0e2e6479fec0b6047810744b
Vortex-oo <debjit.github@gmail.com>
2025-03-11 19:43:09 +0530

    fix the menu button

--- src/component/navbar2.jsx
+++ src/component/navbar2.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion';
 import { FaPlus } from "react-icons/fa";
+
 const Navbar2 = () => {
-  return (
-    <div>
-        <div className='flex gap-1 justify-center items-center cursor-pointer'>
-            <h1 className='text-base font-bold'  >
-                Menu
-            </h1>
-            <div className='bg-white text-black font-black  rounded-full w-3 h-3  p-2 flex justify-center items-center '  >
-                <h1 className='text-base font-extrabold'  ><FaPlus /></h1>
+
+    const [plusClicked, setPlusClicked] = useState('default')
+
+    const variants = {
+        default: {
+            rotate: 0,
+        },
+        clicked: {
+            rotate: 45,
+        }
+    }
+
+    return (
+        <div
+        >
+            <div className='flex gap-1 justify-center items-center cursor-pointer'>
+                <h1 className='text-base font-bold'  >
+                    Menu
+                </h1>
+                <motion.div
+
+                    variants={variants}
+                    onClick={() => setPlusClicked(!plusClicked)}
+                    className='bg-white text-black font-black  rounded-full w-3 h-3  p-2 flex justify-center items-center '
+                    animate={
+                        plusClicked ? 'default' : 'clicked'
+                    }
+                    transition={{
+                        duration: 0.5
+                    }} >
+                    <h1 className='text-base font-extrabold'  ><FaPlus /></h1>
+                </motion.div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar2

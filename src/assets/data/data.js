8eadf6e50870c9529c9c984e0c6fbb6f718dd6c8
vortex <debjit.github@gmail.com>
2025-09-05 19:41:26 +0000

    add new skills

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,23 +1,27 @@
 export const data = [
     {
-        name:"HTML",
-        src:"https://utfs.io/f/72043051-ada7-45a2-a0fc-07c68c73e264-1br8r.png"
+        name: "HTML",
+        src: "https://utfs.io/f/72043051-ada7-45a2-a0fc-07c68c73e264-1br8r.png"
     },
     {
-        name:"CSS",
+        name: "CSS",
         src: "https://utfs.io/f/4c3299a2-4985-4440-aaf9-b7b82fb130d3-1fqb.png"
     },
     {
-        name:"Java Script",
+        name: "Java Script",
         src: "https://utfs.io/f/db14137f-9a8f-482d-b6cf-2799923de4ae-2mh.png"
     },
     {
-        name:"React",
-        src:"https://utfs.io/f/037b5efb-17d7-4c44-90df-d32a17e8f052-1sj3pb.png"
+        name: "React",
+        src: "https://utfs.io/f/037b5efb-17d7-4c44-90df-d32a17e8f052-1sj3pb.png"
     },
     {
         name: "Node JS",
         src: "https://utfs.io/f/334e8469-eac1-40f3-a4fd-6d9385ebe20f-20lc2.png"
+    },
+    {
+        name: "Next Js",
+        src: "https://res.cloudinary.com/dooekcvv0/image/upload/v1757101077/xzolvhvu0unzsgnknk9w.png"
     },
     {
         name: "Mongo DB",
@@ -28,11 +32,24 @@ export const data = [
         src: "https://utfs.io/f/413ae006-bcf6-4e2a-b950-feb8c0d297d0-2idi.png"
     },
     {
+        name: "Postman",
+        src: "https://res.cloudinary.com/dooekcvv0/image/upload/v1757101150/dleorrugbrcpr6tfrk6f.png"
+    },
+    {
         name: "Git",
         src: "https://utfs.io/f/33176dc8-13c2-465e-888b-9f7eaf37cb14-26z6.png"
     },
     {
         name: "Github",
         src: "https://utfs.io/f/fd067e2d-67ef-49f7-a986-69c7c390b2e9-kfrkx3.png"
+    },
+    {
+        name: "AWS",
+        src: "https://res.cloudinary.com/dooekcvv0/image/upload/v1757100879/ismovpvixqh1emqogzay.png"
+    },
+    {
+        name: "Docker",
+        src: "https://res.cloudinary.com/dooekcvv0/image/upload/v1757100954/bwusln0oorulim9v0o4j.png"
     }
+
 ]
